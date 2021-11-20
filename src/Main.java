d6106d285d4d4d0fb8d3f485505cb03051881d40
Joanna <joannabaldyga1994@gmail.com>
2021-11-20 11:14:05 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -16,19 +16,42 @@ public class Main {
 //        for(String car:cars){
 //            System.out.println(car);
 //    };
+//
+//    int day = 3;
+//    switch (day){
+//        case 1:
+//            System.out.println("Monday");
+//            break;
+//        case 2:
+//            System.out.println("Tuesday");
+//            break;
+//        default:
+//            System.out.println("Weekend");
+//
+//        }
+//
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        int j = 0;
+//        do{
+//            System.out.println(j);
+//            j++;
+//        }
+//        while (j<5);
+//    }
 
-    int day = 3;
-    switch (day){
-        case 1:
-            System.out.println("Monday");
-            break;
-        case 2:
-            System.out.println("Tuesday");
-            break;
-        default:
-            System.out.println("Weekend");
-
-
+        for (int i = 0; i < 10; i++) {
+            if(i == 5){
+                int x;
+                x = i + 5;
+                System.out.println(x);
+                continue;
+            }
+            System.out.println(i);
         }
     }
     }
