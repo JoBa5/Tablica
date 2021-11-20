5bff01dd77ebad331a496f54b9d231fcd8ff714e
Joanna <joannabaldyga1994@gmail.com>
2021-11-20 10:46:19 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         int [] tablica = {1,2,3,4,5,6};
-        for(int i = 0; i < tablica.length; i++){
-            if( tablica [i] < 3)
-                System.out.println("True");
-            else if (tablica[i] < 5 )
-                    System.out.println("False");
-            }
+        for (int liczba:tablica) {
+            if(liczba > 3 && liczba < 5)
+            System.out.println(liczba);
+            
+        }
+//        for(int i = 0; i < tablica.length; i++){
+//            if( tablica [i] < 3)
+//                System.out.println("True");
+//            else if (tablica[i] < 5 )
+//                    System.out.println("False");
+//            }
+//        String [] cars ={"audi", "bmw", "vw", "volvo"};
+//        for(String car:cars){
+//            System.out.println(car);
+        }
     }
-}
+
